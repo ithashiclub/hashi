c48ecea1fa5678cde1ff890010607e4f696c9052
ithashiclub <102993293+ithashiclub@users.noreply.github.com>
2022-04-05 13:56:01 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+import Navbar from './components/Navbar.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home  from './pages/Home';
+
+
+
+ 
+
+function App() {
+  
+  return (
+    <div className="App"onClick={Navbar.closeMenu}>
+      <Router>
+      
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home}/> 
+        </Switch>
+          
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
